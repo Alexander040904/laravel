a0f64d3b7635ed015560ac0ac812a0bed8e9f4a2
Alexander090409 <jonathanalexanderramirezortega@gmail.com>
2024-12-01 14:54:55 -0600

    ✨ feat(database): Seeders
    Crear seeder idependiente para post y usuario</br>

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $post = new Post();
+        $post->title ='La cueva negra';
+        $post->content = 'Cd';
+        $post->category = 'Comedy';
+        $post->save();
+
+        $post = new Post();
+        $post->title ='The fash furius';
+        $post->content = 'Cd';
+        $post->category = 'Acction';
+        $post->published_at = now();
+        $post->save();
+    }
+}
